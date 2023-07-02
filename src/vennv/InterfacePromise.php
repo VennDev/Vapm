621be319276dffd4916df6e9cb880ba3e0ebf04c
VennV <111500380+VennDev@users.noreply.github.com>
2023-07-02 23:25:21 +0700

    Update InterfacePromise.php

--- src/vennv/InterfacePromise.php
+++ src/vennv/InterfacePromise.php
@@ -4,14 +4,29 @@ namespace vennv;
 
 interface InterfacePromise {
 
+    /**
+     * This is method getId for get id point of promise
+     */
     public function getId() : int;
 
+    /**
+     * This is method then for add callback to promise
+     */
     public function then(callable $callable) : Promise;
 
+    /**
+     * This is method catch for add callback to promise
+     */
     public function catch(callable $callable) : Promise;
 
+    /**
+     * This is method finally for add callback to promise
+     */
     public static function resolve(mixed $result) : void;
 
+    /**
+     * This is method finally for add callback to promise
+     */
     public static function reject(mixed $result) : void;
 
 }
