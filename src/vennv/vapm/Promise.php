17edab5a7677ee088c125c40b440bd3408b55677
VennV <111500380+VennDev@users.noreply.github.com>
2023-07-11 19:05:07 +0700

    Add files via upload

--- src/vennv/vapm/Promise.php
+++ src/vennv/vapm/Promise.php
@@ -306,10 +306,12 @@ final class Promise implements PromiseInterface
                     if (!is_null($queue1)) 
                     {
                         $queue1->then($callable);
+                        $queue1->catch($this->callbackReject);
                     }
                     elseif (!is_null($queue2))
                     {
                         $queue2->then($callable);
+                        $queue2->catch($this->callbackReject);
                     }
 
                     unset($callbacks[$case]);
