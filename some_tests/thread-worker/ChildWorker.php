cbd99e42c69e57c439adec352764142ac1ff37d4
VennV <111500380+VennDev@users.noreply.github.com>
2023-08-29 01:10:18 +0700

    Add files via upload

---
+++ some_tests/thread-worker/ChildWorker.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use vennv\vapm\simultaneous\Work;
+use vennv\vapm\simultaneous\Worker;
+
+$work1 = new Work();
+$work2 = new Work();
+
+$work1->add(function () : string {
+    // In this will work with threads.
+    return 'Hello World! 1';
+});
+
+$work1->add(function () : string {
+    // In this will work with threads.
+    return 'Hello World! 2';
+});
+
+$work2->add(function () : string {
+    // In this will work with threads.
+    return 'Hello World! 3';
+});
+
+$work2->add(function () : string {
+    // In this will work with threads.
+    return 'Hello World! 4';
+});
+
+$options = ['threads' => 1];
+
+$worker = new Worker($work1, $options);
+$childWorker = new Worker($work2, $options);
+
+// This will add a child worker to the main worker.
+$worker->addWorker($childWorker, function (array $result, Worker $childWorker) : void {
+    $childWorker->done(); // This will void memory leaks
+});
+
+$worker->run(function (array $result, Worker $worker) : void {
+    var_dump($result);
+    $worker->done(); // This will void memory leaks
+});
--- some_tests/thread-worker/TestWorker.php
+++ some_tests/thread-worker/TestWorker.php
@@ -8,19 +8,16 @@ use vennv\vapm\simultaneous\Worker;
 $work = new Work();
 
 $work->add(function () : string {
-    // In this will work with thread.
+    // In this will work with threads.
     return 'Hello World! 1';
 });
 
 $work->add(function () : string {
-    // In this will work with thread.
+    // In this will work with threads.
     return 'Hello World! 2';
 });
 
-$worker = new Worker($work, [
-    'threads' => 1,
-    'max_queue' => 2
-]);
+$worker = new Worker($work, ['threads' => 1]);
 
 $worker->run(function (array $result, Worker $worker) : void {
     var_dump($result);
